b15a9b92eed9e047cdc67d36030d81fd0c5eec77
Yehor Havryliuk <36529973+Jergs@users.noreply.github.com>
2023-08-20 20:06:35 +0300

    Add custom filters: jwt, logging

---
+++ src/main/java/com/havryliuk/yehor/bank/app/demo/filter/RequestValidationBeforeFilter.java
@@ -0,0 +1,50 @@
+package com.havryliuk.yehor.bank.app.demo.filter;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class RequestValidationBeforeFilter extends OncePerRequestFilter {
+
+    private static final String AUTHENTICATION_SCHEME_BASIC = "Basic";
+    private static final Charset CREDENTIALS_CHARSET = StandardCharsets.UTF_8;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String header = request.getHeader(AUTHORIZATION);
+        if (header != null) {
+            header = header.trim();
+            if (StringUtils.startsWithIgnoreCase(header, AUTHENTICATION_SCHEME_BASIC)) {
+                byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
+                byte[] decoded;
+                try {
+                    decoded = Base64.getDecoder().decode(base64Token);
+                    String token = new String(decoded, CREDENTIALS_CHARSET);
+                    int delim = token.indexOf(":");
+                    if (delim == -1) {
+                        throw new BadCredentialsException("Invalid basic authentication token");
+                    }
+                    String email = token.substring(0, delim);
+                    if (email.toLowerCase().contains("test")) {
+                        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        return;
+                    }
+                } catch (IllegalArgumentException e) {
+                    throw new BadCredentialsException("Failed to decode basic authentication token");
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
